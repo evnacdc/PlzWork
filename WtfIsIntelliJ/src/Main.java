db2d61ac276ae42009c7d609f143435e24f7572e
evnacdc <evnacdc@gmail.com>
2017-06-02 18:16:14 -0400

    Initial commit

---
+++ WtfIsIntelliJ/src/Main.java
@@ -0,0 +1,22 @@
+/**
+ * Created by evnac on 6/2/2017.
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello java");
+    }
+
+    public class MyClass
+    {
+        int age;
+        String name;
+        public MyClass(int age, String name)
+        {
+            this.age = age;
+            this.name = name;
+            // here's a comment asdfsdfa
+        }
+    }
+}
